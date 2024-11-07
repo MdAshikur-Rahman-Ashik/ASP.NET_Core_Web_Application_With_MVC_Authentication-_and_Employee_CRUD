33e412dcc70b862b7795963a5c39019ebbb07ebb
Mohammad Ashik <ashikmollah12@gmail.com>
2024-11-07 19:54:04 +0600

    Add project files.

---
+++ CoreWebApp/Program.cs
@@ -0,0 +1,67 @@
+using CoreWebApp.Models;
+using CoreWebApp.Repository;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDBContext>(op => op.UseSqlServer(builder.Configuration.GetConnectionString("con")));
+builder.Services.AddScoped<IEmployeeRepository, EmployeeSQlRepository>();
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 3;
+    options.Password.RequireNonAlphanumeric = false;
+}).AddEntityFrameworkStores<AppDBContext>();
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminUserPolicy", policy =>
+        policy.RequireRole("Admin", "User"));
+});
+
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.AccessDeniedPath = "/Account/AccessDenied";
+});
+
+
+
+var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    // Ensure roles exist
+    if (!await roleManager.RoleExistsAsync("Admin"))
+    {
+        await roleManager.CreateAsync(new IdentityRole("Admin"));
+    }
+
+    if (!await roleManager.RoleExistsAsync("Manager"))
+    {
+        await roleManager.CreateAsync(new IdentityRole("Manager"));
+    }
+}
+
+
+
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllerRoute(name: "Default", pattern: "{Controller=Account}/{action=Login}/{id?}");
+app.Run();
